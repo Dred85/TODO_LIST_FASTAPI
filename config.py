c7e46cd7552f08d01c0e77a99e17ee0a6ed57cc7
Olonov Aleksandr <adrolv08111985@gmail.com>
2025-01-26 17:36:32 +0300

    upd readmy.md

--- config.py
+++ config.py
@@ -2,13 +2,18 @@ from databases import Database
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 class Model(DeclarativeBase):
     pass
 
 
-DATABASE_URL = "postgresql+asyncpg://postgres:qwertyuiop123#@localhost/todo_fastapi"
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 # Создаем асинхронный движок
 async_engine = create_async_engine(DATABASE_URL, echo=True)
